161863e29b7bec9f190079ea193a117ec7058c38
vitrasethy <sethy.vitra@gmail.com>
2023-11-20 21:44:40 +0700

    fix show project for acc not admin

--- app/api/route.js
+++ app/api/route.js
@@ -4,9 +4,9 @@ import getUserData from "@/components/auth/getUserData";
 export async function GET() {
   const cookieStore = cookies();
   const token = cookieStore.get("access_token");
-  const eventId = cookies().get('event_id')
-  const deptId = cookies().get('dept_id')
-  const yearId = cookies().get('year_id')
+  const eventId = cookies().get("event_id");
+  const deptId = cookies().get("dept_id");
+  const yearId = cookies().get("year_id");
 
   const res = await fetch(
     `https://admin.rupp.support/api/v1/events/${eventId.value}/departments/${deptId.value}/years/${yearId.value}`,
@@ -21,7 +21,7 @@ export async function GET() {
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
+    throw new Error("Failed to fetch data");
   }
 
   const data = await res.json();
@@ -29,25 +29,26 @@ export async function GET() {
 
   let i = 1;
   let newData = [];
-  data.forEach((e) => {
-    const tempData = {
-      no: i,
-      id: e.eve_project_code,
-      project_id: e.eve_shortlist_id,
-      name: e.eve_project_topic,
-      type: e.eve_project_type,
-      supervisor: e.eve_project_supervisor_name,
-      leader: e.eve_project_members[0],
-      member: e.eve_project_members.slice().map((item) => ({
-        name: item.name_latin,
-        gender: item.sys_gender.name_latin,
-      })),
-    };
 
-    let tempCommittee = {};
-    let status = 0;
+  if (userData.is_admin) {
+    data.forEach((e) => {
+      let combinedData;
+      const tempData = {
+        no: i,
+        id: e.eve_project_code,
+        project_id: e.eve_shortlist_id,
+        name: e.eve_project_topic,
+        type: e.eve_project_type,
+        supervisor: e.eve_project_supervisor_name,
+        leader: e.eve_project_members[0],
+        member: e.eve_project_members.slice().map((item) => ({
+          name: item.name_latin,
+          gender: item.sys_gender.name_latin,
+        })),
+      };
 
-    if (userData.is_admin) {
+      let tempCommittee;
+      let status;
       let temp = [];
       e.eve_project_committee.map((x) => {
         if (x.is_evaluated) temp.push(true);
@@ -63,28 +64,61 @@ export async function GET() {
         total_score: 98,
         committee: e.eve_project_committee,
       };
-    } else {
-      const userId = userData.eve_committee_id;
-      eve_project_committee
-        .filter((y) => y.id === userId)
-        .map(
-          (y) =>
-            (tempCommittee = {
-              status: y.is_evaluated,
-              total_score: y.project_score,
-              committee: null,
-            }),
-        );
-    }
-    const combinedData = {
-      ...tempData,
-      ...tempCommittee,
-    };
+      combinedData = {
+        ...tempData,
+        ...tempCommittee,
+      };
 
-    newData.push(combinedData)
-    i++
-  });
+      newData.push(combinedData);
+      i++;
+    });
+  } else {
+    const filterData = data.filter((project) => {
+      return project.eve_project_committee.some(
+        (committeeMember) => committeeMember.id === userData.eve_committee_id,
+      );
+    });
+    filterData.forEach((e) => {
+      let combinedData;
+      const tempData = {
+        no: i,
+        id: e.eve_project_code,
+        project_id: e.eve_shortlist_id,
+        name: e.eve_project_topic,
+        type: e.eve_project_type,
+        supervisor: e.eve_project_supervisor_name,
+        leader: e.eve_project_members[0],
+        member: e.eve_project_members.slice().map((item) => ({
+          name: item.name_latin,
+          gender: item.sys_gender.name_latin,
+        })),
+      };
 
+      let tempCommittee;
+      let status;
+      if (
+        e.eve_project_committee.find((p) => p.id === userData.eve_committee_id)
+          .is_evaluated
+      )
+        status = 1;
+      else status = 0;
+
+      tempCommittee = {
+        status: status,
+        total_score: e.eve_project_committee.find(
+          (p) => p.id === userData.eve_committee_id,
+        ).project_score,
+        committee: [],
+      };
+      combinedData = {
+        ...tempData,
+        ...tempCommittee,
+      };
+
+      newData.push(combinedData);
+      i++;
+    });
+  }
 
   return Response.json(newData);
 }
