63d651da8906b88e6228c087137e39e6ecf35062
sunnengsen <sunnengsen@gmail.com>
2023-11-23 10:25:29 +0700

    alredy

--- app/api/detail/route.js
+++ app/api/detail/route.js
@@ -5,7 +5,7 @@ export async function GET() {
     const token = cookieStore.get("access_token");
     const projectId = cookies().get("project_id");
 
-    const res = await fetch(`https://admin.rupp.support/api/v1/events/project-shortlist-details/${projectId.value}`, {
+    const res = await fetch(`https://admin.rupp.support/api/v1/events/project-shortlist-committee-details/1`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
--- app/events/[deptyear]/projects/projectDetail/page.jsx
+++ app/events/[deptyear]/projects/projectDetail/page.jsx
@@ -32,74 +32,47 @@ export default function ProjectTable({}) {
 
     fetchData();
   }, []);
+  function renderMember(index) {
+    if (index === 0) {
+      return "hidden";
+    }
+    return "";
+  }
 
-  const datas = data.committees;
-  console.log(datas);
   return (
     <div className=" flex flex-col justify-center items-center">
       <h1 className="text-center font-extrabold text-5xl my-10">
         Projects Detail
       </h1>
-      <p>{
-        datas?.map((datass) => (
-          <div key={datass.no}>
-            <p className="text-center font-extrabold text-3xl my-10">
-              {datass.projects?.map((project) => (
-                <p key={project.id}>
-                  <p>
-                    {project.code}
-                  </p>
-                  <p>
-                    {project.name}
-                  </p>
-                  <p>
-                    {project.total_score}
-                  </p>
-                  
-                </p>
-                
-              ))}
-            </p>
-          </div>
-        ))
-      }</p>
 
-
-
-
-
-
-
-
-
-      {/* {data.map((data) => (
+      {data?.map((data) => (
         <p key={data.no} className="px-10 py-5 text-lg border-2 w-[60%] ">
           <div className="bg-gray-200 p-2 rounded-md mt-2">
             <p className="text-sm text-gray-500 uppercase">Project Name</p>
-            <p>{data.name}</p>
+            <p>{data.eve_project_topic}</p>
           </div>
           <div className="bg-gray-200 p-2 rounded-md mt-2">
             <p className="text-sm text-gray-500 uppercase">Project ID</p>
-            <p>{data.id}</p>
+            <p>{data.eve_project_code}</p>
           </div>
           <div className="bg-gray-200 p-2 rounded-md mt-2">
             <p className="text-sm text-gray-500 uppercase">Project Leader</p>
-            <p>{data.leader.name_latin}</p>
+            <p>{data.eve_project_members[0].name_latin}</p>
           </div>
           <div className="bg-gray-200 p-2 rounded-md mt-2">
-            {data.member.map((members, index) => (
-              <p key={members.id}>
+            {data.eve_project_members.map((members, index) => (
+              <p key={members.id} className={renderMember(index)}>
                 <p className="text-sm text-gray-500 uppercase">
-                  {index === 0 ? "Project Member" : ""}
+                  {index === 1 ? "Project Member" : ""}
                 </p>
 
-                <li>{members.name}</li>
+                <li>{members.name_latin}</li>
               </p>
             ))}
           </div>
 
           <div className="bg-gray-200 p-2 rounded-md mt-2">
-            {data.committee.map((committee, index) => (
+            {data.eve_project_committee.map((committee, index) => (
               <p key={committee.id} onClick={() => handleClick(committee.id)}>
                 <p className="text-sm text-gray-500 uppercase">
                   {index === 0 ? "Judge" : ""}
@@ -107,71 +80,43 @@ export default function ProjectTable({}) {
 
                 <p>
                   <p className="border flex justify-between px-10 bg-gray-100 border-gray-300 w-auto my-1 rounded-md p-2 cursor-pointer hover:bg-gray-300 duration-300">
-                    <p>
-
-                    {committee.name}
-                    </p>
+                    <p>{committee.name}</p>
                     <p className="flex">
-                      <p className="mx-10">
-
-                    {committee.project_score}
-                      </p>
-                    <Image className="" src={arrow} alt="arrow" width={20} height={20} />
+                      <p className="mx-10">{"Point : "}{committee.project_score}</p>
+                      <Image
+                        className={`${
+                          openDropdowns.includes(committee.id)
+                            ? "rotate-180"
+                            : ""
+                        } duration-300`}
+                        src={arrow}
+                        alt="arrow"
+                        width={15}
+                        height={20}
+                      />
                     </p>
                   </p>
                   <div>
-                  {openDropdowns.includes(committee.id) && (
+                    {openDropdowns.includes(committee.id) && (
                       <div className="border border-gray-300 rounded-md px-10 py-5 bg-white">
-                        <div className="my-5 border-2 border-gray-300 flex justify-between py-1 px-5 rounded-lg bg-gray-200 items-center">
+                        <div>
                           <h1>
-                            Introduction / Background / literature (clear)
+                            {committee.categories.map((categories) => (
+                              <p key={categories.id}>
+                                {categories.criterias.map((criterias) => (
+                                  <p
+                                    key={criterias.id}
+                                    className="my-5 border-2 border-gray-300 flex justify-between py-1 px-5 rounded-lg bg-gray-200 items-center"
+                                  >
+                                    <p>{criterias.name}</p>
+                                    <p className={`ml-5 border border-black rounded-full bg-[#014194] text-white ${criterias.score < 10 ? 'px-3 py-1':'px-2 py-1'}`}>
+                                      {criterias.score}
+                                    </p>
+                                  </p>
+                                ))}
+                              </p>
+                            ))}
                           </h1>
-                          <p className="text-red-500 px-2 ml-5 py-[3px]">
-                            10 
-                          </p>
-                        </div>
-                        <div className="my-5 border-2 border-gray-300 flex justify-between py-1 px-5 rounded-lg bg-gray-200 items-center">
-                          <h1>Objective clearly stated & concise</h1>
-                          <p className="text-red-500 px-2 ml-5 py-[3px]">
-                            10 
-                          </p>
-                        </div>
-                        <div className="my-5 border-2 border-gray-300 flex justify-between py-1 px-5 rounded-lg bg-gray-200 items-center">
-                          <h1>
-                            Approach / methodd / study design / Materials (new,
-                            clear & concise)
-                          </h1>
-                          <p className="text-red-500 px-2 ml-5 py-[3px]">
-                            10 
-                          </p>
-                        </div>
-                        <div className="my-5 border-2 border-gray-300 flex justify-between py-1 px-5 rounded-lg bg-gray-200 items-center">
-                          <h1>
-                            Novel approach / method / study design / Materials
-                          </h1>
-                          <p className="text-red-500 px-2 ml-5 py-[3px]">
-                            10 
-                          </p>
-                        </div>
-                        <div className="my-5 border-2 border-gray-300 flex justify-between py-1 px-5 rounded-lg bg-gray-200 items-center">
-                          <h1>Analysis and Interpretation</h1>
-                          <p className="text-red-500 px-2 ml-5 py-[3px]">
-                            10 
-                          </p>
-                        </div>
-                        <div className="my-5 border-2 border-gray-300 flex justify-between py-1 px-5 rounded-lg bg-gray-200 items-center">
-                          <h1>Demonstrate signi</h1>
-                          <p className="text-red-500 px-2 ml-5 py-[3px]">
-                            10 
-                          </p>
-                        </div>
-                        <div className="my-5 border-2 border-gray-300 flex justify-between py-1 px-5 rounded-lg bg-gray-200 items-center">
-                          <h1>
-                            Introduction / Background / literature (clear)
-                          </h1>
-                          <p className="text-red-500 px-2 ml-5 py-[3px]">
-                            10 
-                          </p>
                         </div>
                       </div>
                     )}
@@ -180,10 +125,8 @@ export default function ProjectTable({}) {
               </p>
             ))}
           </div>
-
-
         </p>
-      ))} */}
+      ))}
     </div>
   );
 }
