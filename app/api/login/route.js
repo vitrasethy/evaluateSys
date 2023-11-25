a0556379acca5546b4c4b0e44a9b0460dc4de58a
vitrasethy <sethy.vitra@gmail.com>
2023-11-25 20:09:01 +0700

    login

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,27 +1,30 @@
 import {cookies} from "next/headers";
 
 export async function POST(request) {
-    const resFront = await request.json()
-    const data = {
-        username: resFront.username,
-        password: resFront.password
-    }
-    const res = await fetch("https://admin.rupp.support/api/v1/auth/login", {
-        method: "POST",
-        cache: 'no-store',
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        body: JSON.stringify(data),
-    });
-    if (!res.ok) {
-        throw new Error("Failed to fetch data");
-    }
+  const resFront = await request.json()
+  const data = {
+    username: resFront.username,
+    password: resFront.password
+  }
+  const res = await fetch("https://admin.rupp.support/api/v1/auth/login", {
+    method: "POST",
+    cache: 'no-store',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-    const resJson = await res.json();
+  const resJson = await res.json();
 
-    cookies().set('jwt', resJson.access_token)
+  cookies().set('jwt', resJson.access_token, {
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true
+  })
 
-    return Response.json({ message: "hehe" })
+  return Response.json({message: "hehe"})
 }
