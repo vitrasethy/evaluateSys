480508309d104bdeefcc72d9e599a8c9b1c466cc
vitrasethy <sethy.vitra@gmail.com>
2023-11-26 11:09:20 +0700

    login

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -24,7 +24,6 @@ export async function POST(request) {
   cookies().set('jwt', resJson.access_token, {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    sameSite: strict
   })
 
   return Response.json({message: ""})
