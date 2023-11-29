def2b6958cc071d698de5aaa2e758d50ab9e969c
vitrasethy <sethy.vitra@gmail.com>
2023-11-29 20:14:05 +0700

    fix fix

--- components/nav-bar/NavBar.jsx
+++ components/nav-bar/NavBar.jsx
@@ -10,7 +10,7 @@ export default async function Navbar() {
   const userData = await getUserData()
   const isAdmin = userData["is_admin"]
 
-  async function action(){
+  async function action() {
     "use server"
 
     cookies().delete('access_token')
@@ -24,7 +24,7 @@ export default async function Navbar() {
         <div className="Desktop text-white h-20 justify-between items-center hidden md:flex mx-5">
           <div>
             <Link href={"/events"}>
-              <Image src="/logoFE.webp" alt="" width={50} height={50} />
+              <Image src="/logoFE.webp" alt="" width={50} height={50}/>
             </Link>
           </div>
           <div className="flex justify-around w-2/2 focus:outline-none focus:ring uppercase">
@@ -36,20 +36,23 @@ export default async function Navbar() {
             >
               Result
             </Link>
-            <button formAction={action}
-              className=" hover:bg-white hover:text-black px-5 py-6 transition ease-in-out delay-50 focus:outline-none focus:ring"
+            <form action={action}>
+              <button
+                className=" hover:bg-white hover:text-black px-5 py-6 transition ease-in-out delay-50 focus:outline-none focus:ring"
 
-            >
-              Logout
-            </button>
+              >
+                LOGOUT
+              </button>
+            </form>
           </div>
         </div>
-        <div className="Mobile text-white flex h-20 items-center ml-5 justify-between md:hidden transition duration-200 z-50">
+        <div
+          className="Mobile text-white flex h-20 items-center ml-5 justify-between md:hidden transition duration-200 z-50">
           <Link href={"/events"}>
-            <Image src="/logoFE.webp" alt="" width={50} height={50} />
+            <Image src="/logoFE.webp" alt="" width={50} height={50}/>
           </Link>
 
-          <Hamburger isAdmin={isAdmin} />
+          <Hamburger isAdmin={isAdmin}/>
         </div>
       </div>
     </div>
