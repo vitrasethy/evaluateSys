9eca5a5921282e8e56feb63e8977e5d4758a9a7c
vitrasethy <sethy.vitra@gmail.com>
2023-11-29 18:39:50 +0700

    fix fix

--- middleware.js
+++ middleware.js
@@ -27,21 +27,21 @@ export async function middleware(request) {
     // }
 
     if (request.nextUrl.pathname === "/" && hasCookie) {
-        const token = cookies().get("access_token");
-        const res = await fetch("https://admin.rupp.support/api/v1/auth/protected", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-                Authorization: "Bearer " + token?.value,
-            },
-        });
-
-        if (!res.ok) {
-            const response = NextResponse.rewrite(new URL("/", request.url));
-            response.cookies.delete("access_token");
-            return NextResponse.rewrite(new URL("/", request.url));
-        }
+        // const token = cookies().get("access_token");
+        // const res = await fetch("https://admin.rupp.support/api/v1/auth/protected", {
+        //     method: "GET",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         Accept: "application/json",
+        //         Authorization: "Bearer " + token?.value,
+        //     },
+        // });
+        //
+        // if (!res.ok) {
+        //     const response = NextResponse.rewrite(new URL("/", request.url));
+        //     response.cookies.delete("access_token");
+        //     return NextResponse.rewrite(new URL("/", request.url));
+        // }
 
         return NextResponse.rewrite(new URL("/events", request.url));
     }
