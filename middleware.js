93a86665e578a59d250aa71078502eeaf2f3b265
vitrasethy <sethy.vitra@gmail.com>
2023-11-29 20:03:10 +0700

    fix fix

--- middleware.js
+++ middleware.js
@@ -5,14 +5,14 @@ export async function middleware(request) {
     const cookiesList = cookies();
     const hasCookie = cookiesList.has("access_token");
 
-    if (request.nextUrl.pathname.startsWith("/logout") && !hasCookie) {
-        return NextResponse.rewrite(new URL("/", request.url));
-    } else if (request.nextUrl.pathname.startsWith("/logout")) {
-        request.cookies.clear();
-        const response = NextResponse.rewrite(new URL("/", request.url));
-        response.cookies.delete("access_token");
-        return response;
-    }
+    // if (request.nextUrl.pathname.startsWith("/logout") && !hasCookie) {
+    //     return NextResponse.rewrite(new URL("/", request.url));
+    // } else if (request.nextUrl.pathname.startsWith("/logout")) {
+    //     request.cookies.clear();
+    //     const response = NextResponse.rewrite(new URL("/", request.url));
+    //     response.cookies.delete("access_token");
+    //     return response;
+    // }
 
     // if (request.nextUrl.pathname.startsWith("/login") && hasCookie) {
     //     return NextResponse.rewrite(new URL("/events", request.url));
