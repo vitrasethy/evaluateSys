3e446e7276b2405b5040b93a2a0d0d0c936b84f0
sunnengsen <sunnengsen@gmail.com>
2023-12-01 09:22:24 +0700

    more style

--- app/events/[deptyear]/projects/projectDetail/page.jsx
+++ app/events/[deptyear]/projects/projectDetail/page.jsx
@@ -38,22 +38,26 @@ export default function ProjectTable({}) {
     }
     return "";
   }
+  console.log(data);
 
   return (
     <div className=" flex flex-col justify-center items-center">
       <h1 className="text-center font-extrabold text-5xl my-10">
         Projects Detail
       </h1>
-
       {data?.map((data) => (
-        <p key={data.no} className="px-10 py-5 text-lg border-2 w-[90%] sm:w-[70%] md:w-[60%] duration-500">
-          <div className="bg-gray-200 p-2 rounded-md mt-2">
+        <p key={data.no} className="md:px-10 px-2 py-2 md:py-10 text-lg border-2 w-[90%] sm:w-[70%] md:w-[60%] duration-500">
+          <div className="bg-gray-200 p-2 rounded-md">
             <p className="text-sm text-gray-500 uppercase">Project Name</p>
             <p>{data.eve_project_topic}</p>
           </div>
           <div className="bg-gray-200 p-2 rounded-md mt-2">
             <p className="text-sm text-gray-500 uppercase">Project ID</p>
             <p>{data.eve_project_code}</p>
+          </div>
+          <div className="bg-gray-200 p-2 rounded-md mt-2">
+            <p className="text-sm text-gray-500 uppercase">Project Supervisor</p>
+            <p>{data.eve_project_supervisor_name}</p>
           </div>
           <div className="bg-gray-200 p-2 rounded-md mt-2">
             <p className="text-sm text-gray-500 uppercase">Project Leader</p>
@@ -65,8 +69,7 @@ export default function ProjectTable({}) {
                 <p className="text-sm text-gray-500 uppercase">
                   {index === 1 ? "Project Member" : ""}
                 </p>
-
-                <li>{members.name_latin}</li>
+                <li className="ml-5">{members.name_latin}</li>
               </p>
             ))}
           </div>
@@ -79,16 +82,16 @@ export default function ProjectTable({}) {
                 </p>
 
                 <p>
-                  <p className="border flex justify-between px-10 bg-gray-100 border-gray-300 w-auto my-1 rounded-md p-2 cursor-pointer hover:bg-gray-300 duration-300">
+                  <p className="border flex justify-between px-5 bg-gray-100 border-gray-300 w-auto my-1 rounded-md p-2 cursor-pointer hover:bg-gray-300 duration-300">
                     <p>{committee.name}</p>
                     <p className="flex">
-                      <p className="mx-10">{"Point : "}{committee.project_score}</p>
+                      <p className="sm:mx-3">{"Point : "}{committee.project_score}</p>
                       <Image
                         className={`${
                           openDropdowns.includes(committee.id)
                             ? "rotate-180"
                             : ""
-                        } duration-300`}
+                        } duration-300 mx-3`}
                         src={arrow}
                         alt="arrow"
                         width={15}
@@ -98,7 +101,7 @@ export default function ProjectTable({}) {
                   </p>
                   <div>
                     {openDropdowns.includes(committee.id) && (
-                      <div className="border border-gray-300 rounded-md px-10 py-5 bg-white">
+                      <div className="border border-gray-300 rounded-md px-3 md:px-10 md:py-5 bg-white">
                         <div>
                           <h1>
                             {committee.categories.map((categories) => (
