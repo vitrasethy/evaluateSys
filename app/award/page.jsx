ea723ab45485cb3f3acf2a395c9a80363503a09c
J3llyFi5h <lovely18012004@gmail.com>
2023-12-05 18:53:26 +0700

    pdf

--- app/award/page.jsx
+++ app/award/page.jsx
@@ -32,17 +32,17 @@ export default function Award() {
     function handleKeyDown(event) {
       if (event.key === "Enter") {
         enterCount.current++;
-        if (enterCount.current=== 1){
+        if (enterCount.current === 1) {
           window.scroll({
             top: document.body.offsetHeight,
-            left: 0, 
+            left: 0,
             behavior: 'smooth',
           });
         }
         if (enterCount.current === 2) {
           setIsLoading(true);
         }
-        if (enterCount.current === 3){
+        if (enterCount.current === 3) {
           setIsLoading2(true);
         }
       }
@@ -55,119 +55,119 @@ export default function Award() {
   return (
     <main>
       <div className="bg-[#070707]">
-      <section className="first-screen">
-      <div id="particle-container">{particles}</div>
+        <section className="first-screen">
+          <div id="particle-container">{particles}</div>
 
-        <div className="text-center h-[85vh] flex flex-col justify-center items-center">
-          <h1 className="text-4xl tracking-tight font-extrabold font-['Georgia'] uppercase sm:text-5xl md:text-[10rem] bg-gradient-to-r from-[#3b7cc1] to-[#9ae2ff] inline-block text-transparent bg-clip-text drop-shadow-[7px_5px_rgba(205,245,253,0.2)]">
-            Engineering&apos;s Day
-          </h1>
-          <p className="max-w-4xl mx-auto text-6xl mt-8 text-white leading-snug">
-            Award of Engineering&apos;s Day 2023
-          </p>
-          <p className="max-w-4xl mx-auto text-5xl mt-8 text-white">
-            Congratulation to the winners...
-          </p>
-        
+          <div className="text-center h-[85vh] flex flex-col justify-center items-center">
+            <h1 className="text-4xl tracking-tight font-extrabold font-['Georgia'] uppercase sm:text-5xl md:text-[10rem] bg-gradient-to-r from-[#3b7cc1] to-[#9ae2ff] inline-block text-transparent bg-clip-text drop-shadow-[7px_5px_rgba(205,245,253,0.2)]">
+              Engineering&apos;s Day
+            </h1>
+            <p className="max-w-4xl mx-auto text-6xl mt-8 text-white leading-snug">
+              Award of Engineering&apos;s Day 2023
+            </p>
+            <p className="max-w-4xl mx-auto text-5xl mt-8 text-white">
+              Congratulation to the winners...
+            </p>
+
+          </div>
+        </section>
+
+        <div id="particle-container">{particles}
         </div>
-      </section>
+        <div className="h-screen"></div>
 
-      <div id="particle-container">{particles}
-      </div>
-      <div className="h-screen"></div>
-
-      <section className="second-screen">
-        <div className="pyro">
-          <div className="before"></div>
-          <div className="after"></div>
-        </div>
-        <div className=" h-screen flex flex-col justify-center items-center">
-          <div className="text-white flex justify-center items-center gap-16 mb-16">
-          <Image
+        <section className="second-screen">
+          <div className="pyro">
+            <div className="before"></div>
+            <div className="after"></div>
+          </div>
+          <div className=" h-screen flex flex-col justify-center items-center">
+            <div className="text-white flex justify-center items-center gap-16 mb-16">
+              <Image
                 src="/bgcol.png"
                 alt=""
                 width={600}
                 height={600}
                 className=""
               />
-            <h1 className="text-7xl font-semibold">Engineering&apos;s Day 2023</h1>
-          </div>
-          <div className="w-[85%] h-[65%] bg-[url('/reward3.jpeg')] bg-no-repeat bg-cover border-2 border-gray-500">
-            <div className="flex flex-col justify-center h-full rounded-md bg-sky-950/30 backdrop-brightness-[.6] w-full px-20">
-              <div className="text-white text-center mb-12">
-              <h3 className="text-7xl font-medium">Leader Board</h3>
-            <h2 className="mt-8 mb-8 text-5xl">Department : Information Technology Engineering</h2>
-            <h2 className="text-5xl">Year: 3</h2>
-              </div>
-
-            <div className="relative w-auto">
-              <div className="flex bg-gradient-to-r from-[#cc9910] to-[#fcf97c] shadow-lg border-1 py-6 px-[4rem] rounded-xl h-[11rem]">
-                <div className="flex gap-16 text-4xl items-center">
-                  <p>1st</p>
-                  {isLoading2&&(
-                    <div className="flex gap-16 items-center">
-                      <p className="w-[102.4rem] leading-snug">GASTROMOD: AN INTERACTIVE TOOL FOR 3D GASTROPOD MODELING</p>
-                      <p>003</p>
-                    </div>
-                  )}
-                </div>
-              </div>
-              <Image
-                src="/gold.png"
-                alt=""
-                width={70}
-                height={80}
-                className="absolute top-[-10px] left-[-25px] rotate-12"
-              />
+              <h1 className="text-7xl font-semibold">Engineering&apos;s Day 2023</h1>
             </div>
+            <div className="w-[85%] h-[65%] bg-[url('/reward3.jpeg')] bg-no-repeat bg-cover border-2 border-gray-500">
+              <div className="flex flex-col justify-center h-full rounded-md bg-sky-950/30 backdrop-brightness-[.6] w-full px-20">
+                <div className="text-white text-center mb-12">
+                  <h3 className="text-7xl font-medium">Leader Board</h3>
+                  <h2 className="mt-8 mb-8 text-5xl">Department : Information Technology Engineering</h2>
+                  <h2 className="text-5xl">Year: 3</h2>
+                </div>
 
-            <div className="relative mt-8">
-              <div className="flex bg-gradient-to-r from-[#C0C0C0] to-[#e9e9eb] shadow-lg border-1 py-6 px-[4rem] rounded-xl h-[11rem]">
-                <div className="flex gap-16 text-4xl items-center">
-                  <p>2nd</p>
-                  {isLoading&&(
-                    <div className="flex gap-16 items-center">
-                      <p className="w-[101.4rem] leading-snug">ASEAN FACTORI 4.0 PROJECT ASEAN FACTORI 4.0 PROJECT</p>
-                      <p>003</p>
+                <div className="relative w-auto">
+                  <div className="flex bg-gradient-to-r from-[#cc9910] to-[#fcf97c] shadow-lg border-1 py-6 px-[4rem] rounded-xl h-[11rem]">
+                    <div className="flex gap-16 text-4xl items-center">
+                      <p>1st</p>
+                      {isLoading2 && (
+                        <div className="flex gap-16 items-center">
+                          <p className="w-[102.4rem] leading-snug">GASTROMOD: AN INTERACTIVE TOOL FOR 3D GASTROPOD MODELING</p>
+                          <p>003</p>
+                        </div>
+                      )}
                     </div>
-                  )}
                   </div>
-              </div>
-              <Image
-                src="/silver.png"
-                alt=""
-                width={70}
-                height={80}
-                className="absolute top-[-10px] left-[-25px] rotate-12"
-              />
-            </div>
-
-            <div className="relative mt-8">
-              <div className="flex bg-gradient-to-r from-[#ca6533] to-[#F0C9BA] shadow-lg border-1 py-6 px-[4rem] rounded-xl h-[11rem]">
-                <div className="flex gap-16 text-4xl items-center w-full">
-                  <p>3rd</p>
-                  {isLoading2&&(
-                    <div className="flex gap-16 items-center">
-                      <p className="w-[102rem] leading-snug">A NEW MODEL FOR SIMULATING AND EVALUATING CONGESTION CAUSE AT SIGNALIZED INTERSECTION</p>
-                      <p>003</p>
-                    </div>
-                  )}
+                  <Image
+                    src="/gold.png"
+                    alt=""
+                    width={70}
+                    height={80}
+                    className="absolute top-[-10px] left-[-25px] rotate-12"
+                  />
                 </div>
-              </div>
-              <Image
-                src="/bronze.png"
-                alt=""
-                width={70}
-                height={80}
-                className="absolute top-[-10px] left-[-25px] rotate-12"
-              />
-            </div>
 
-            <p className="text-center text-3xl mt-14 text-white">Royal University of Phnom Penh || Faculty of Engineering</p>
-            </div>
+                <div className="relative mt-8">
+                  <div className="flex bg-gradient-to-r from-[#C0C0C0] to-[#e9e9eb] shadow-lg border-1 py-6 px-[4rem] rounded-xl h-[11rem]">
+                    <div className="flex gap-16 text-4xl items-center">
+                      <p>2nd</p>
+                      {isLoading && (
+                        <div className="flex gap-16 items-center">
+                          <p className="w-[101.4rem] leading-snug">ASEAN FACTORI 4.0 PROJECT ASEAN FACTORI 4.0 PROJECT</p>
+                          <p>003</p>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                  <Image
+                    src="/silver.png"
+                    alt=""
+                    width={70}
+                    height={80}
+                    className="absolute top-[-10px] left-[-25px] rotate-12"
+                  />
+                </div>
+
+                <div className="relative mt-8">
+                  <div className="flex bg-gradient-to-r from-[#ca6533] to-[#F0C9BA] shadow-lg border-1 py-6 px-[4rem] rounded-xl h-[11rem]">
+                    <div className="flex gap-16 text-4xl items-center w-full">
+                      <p>3rd</p>
+                      {isLoading2 && (
+                        <div className="flex gap-16 items-center">
+                          <p className="w-[102rem] leading-snug">A NEW MODEL FOR SIMULATING AND EVALUATING CONGESTION CAUSE AT SIGNALIZED INTERSECTION</p>
+                          <p>003</p>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                  <Image
+                    src="/bronze.png"
+                    alt=""
+                    width={70}
+                    height={80}
+                    className="absolute top-[-10px] left-[-25px] rotate-12"
+                  />
+                </div>
+
+                <p className="text-center text-3xl mt-14 text-white">Royal University of Phnom Penh || Faculty of Engineering</p>
               </div>
+            </div>
           </div>
-      </section>
+        </section>
 
       </div>
     </main>
--- app/pdf/page.jsx
+++ app/pdf/page.jsx
@@ -1,38 +1,99 @@
-'use client'
-import React, { useState, useEffect, useRef } from "react";
-import html2canvas from 'html2canvas';
+"use client";
+import React, { useState, useEffect} from "react";
+import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
-import '../pdf/fonts/fonts.css';
+import "../pdf/fonts/fonts.css";
+const data = [
+  {
+    id: 1,
+    event: {
+      name_latin: "16th Engineering Day",
+      name_khmer: "ទិវាវិស្វកម្មលើកទី៦",
+      year: "2023",
+    },
+    member: {
+      name_latin: "SEN SUNNENG",
+      name_khmer: "សេន ស៊ុនណេង",
+      gender: "male",
+    },
+    project: {
+      project_id: "IG801",
+      rank: 1,
+      project_name: "Aleo Vera: Production & Processing",
+    },
+  },
+  {
+    id: 2,
+    event: {
+      name_latin: "16th Engineering Day",
+      name_khmer: "ទិវាវិស្វកម្មលើកទី៦",
+      year: "2023",
+    },
+    member: {
+      name_latin: "SOKHA RITHY",
+      name_khmer: "សុខា រិទ្ធី",
+      gender: "male",
+    },
+    project: {
+      project_id: "IG802",
+      rank: 2,
+      project_name: "Evaluate System",
+    },
+  },
+  {
+    id: 3,
+    event: {
+      name_latin: "16th Engineering Day",
+      name_khmer: "ទិវាវិស្វកម្មលើកទី៦",
+      year: "2023",
+    },
+    member: {
+      name_latin: "SENG VEHA",
+      name_khmer: "សេង វេហា",
+      gender: "male",
+    },
+    project: {
+      project_id: "IG803",
+      rank: 3,
+      project_name: "E-Books Library",
+    },
+  },
+];
 function HtmlToImageToPdf() {
-  const htmlRef = useRef(null);
   const [isDownloading, setIsDownloading] = useState(false);
   const [isDownloaded, setIsDownloaded] = useState(false);
-  const handleDownload = () => {
-    setIsDownloading(true);
-    if (htmlRef.current) {
-      // convert the html to a png image
-      html2canvas(htmlRef.current).then((canvas) => {
-        // create a pdf document
-        const pdf = new jsPDF("l", "mm", "a4");
-
-        // get the image data
-        const imgData = canvas.toDataURL("image/png");
-
-        // get the width and height of the PDF document
-        // const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfWidth = window.innerWidth * 0.2645833333;
-        const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
-
-        // add the image as a page
-        pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-
-        // download the pdf
-        pdf.save("download.pdf");
-        setIsDownloading(false);
-        setIsDownloaded(true);
-      });
-    }
+  String.prototype.toKhmerDigit = function () {
+    var id = ["០", "១", "២", "៣", "៤", "៥", "៦", "៧", "៨", "៩"];
+    return this.replace(/[0-9]/g, function (w) {
+      return id[+w];
+    });
   };
+
+  const handleDownload = async () => {
+    setIsDownloading(true);
+  const elements = document.querySelectorAll('.myDivClass');
+    const canvases = await Promise.all(Array.from(elements).map(element => html2canvas(element, {scale: 4})));
+
+    const pdf = new jsPDF("l", "mm", "a4");
+
+    canvases.forEach((canvas, index) => {
+      const imgData = canvas.toDataURL('image/jpeg');
+      const pdfWidth = 297;
+      const pdfHeight = 210;
+      // Add the image to the PDF
+      pdf.addImage(imgData, 'JPEG', 0, 0, pdfWidth, pdfHeight);
+
+      // Add a new page for the next image if it's not the last image
+      if (index < canvases.length - 1) {
+        pdf.addPage();
+      }
+    });
+
+    pdf.save('download.pdf');
+    setIsDownloading(false);
+    setIsDownloaded(true);
+  };
+
   useEffect(() => {
     if (isDownloaded) {
       setTimeout(() => setIsDownloaded(false), 2000);
@@ -40,52 +101,100 @@ function HtmlToImageToPdf() {
   }, [isDownloaded]);
   return (
     <div>
-      <div ref={htmlRef}>
-        <section className="bg-[url('/ctf.png')] bg-contain w-[297mm] h-[210mm] bg-no-repeat">
-          <div className="flex flex-col justify-center items-center h-full">
-          <div className="pt-[5rem]">
-            <div className="grid grid-cols-2 gap-[122px]">
-              <div className="flex flex-col justify-center text-center items-center font-['Times New Roman'] gap-1 w-[391px] h-[300px]">
-                <p className="font-extrabold text-[17.5pt] pt-px font-['HelveticaNowMTTextRegular']">6th ENGINEERING DAY</p>
-                <p className="text-[14pt] font-['HelveticaNowMTTextRegular'] font-normal">AWARDS 2023</p>
-                <p className="text-[17.5pt] font-extrabold leading-7 font-['HelveticaNowMTTextRegular']">CERTIFICATE OF APPRECIATION</p>
-                <p className="text-[13pt] font-['serif']">is hereby awarded to</p>
-                <p className=" text-[17.5pt] font-extrabold font-['HelveticaNowMTTextRegular']">MR. SEN SUNNENG</p>
-                <div className="text-[13pt] text-center font-['serif']">
-                  <p className="">in recognition for winning the</p>
-                  <p className="">1st Award for top project entitled</p>
-                  <div className="flex italic">
-                    <p>&quot;Aleo Vera: Production & Processing&quot;</p>
+      <div>
+        {data.map((data) => (
+          <section
+            key={data.id}
+            className="myDivClass bg-[url('/ctf.png')] bg-contain w-[297mm] h-[210mm] bg-no-repeat"
+          >
+            <div className="flex flex-col justify-center items-center h-full">
+              <div className="pt-[5rem]">
+                <div className="grid grid-cols-2 gap-[122px]">
+                  <div className="flex flex-col justify-center text-center items-center font-['Times New Roman'] gap-1 w-[391px] h-[300px]">
+                    <p className="font-extrabold text-[17.5pt] pt-px font-['HelveticaNowMTTextRegular']">
+                      {data.event.name_latin}
+                    </p>
+                    <p className="text-[14pt] font-['HelveticaNowMTTextRegular'] font-normal">
+                      AWARDS {data.event.year}
+                    </p>
+                    <p className="text-[17.5pt] font-extrabold leading-7 font-['HelveticaNowMTTextRegular']">
+                      CERTIFICATE OF APPRECIATION
+                    </p>
+                    <p className="text-[13pt] font-['serif']">
+                      is hereby awarded to
+                    </p>
+                    <p className=" text-[17.5pt] font-extrabold font-['HelveticaNowMTTextRegular']">
+                      MR. {data.member.name_latin}
+                    </p>
+                    <div className="text-[13pt] text-center font-['serif']">
+                      <p className="">in recognition for winning the</p>
+                      <p className="">
+                        {data.project.rank} Award for top project entitled
+                      </p>
+                      <div className="flex italic">
+                        <p>&quot;{data.project.project_name}&quot;</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col justify-center items-center gap-1 w-[391px] h-[300px]">
+                    <p className="font-[KhmerMoulLight] text-[16pt] pt-1">
+                      {data.event.name_khmer}
+                    </p>
+                    <p className="font-[KhmerBTB] text-[13pt]">
+                      ជ័យលាភីឆ្នាំ {data.event.year.toKhmerDigit()}
+                    </p>
+                    <p className="font-[KhmerMoulLight] text-[16pt]">
+                      ប័ណ្ណសរសើរ
+                    </p>
+                    <p className="font-[KhmerBTB] text-[13pt]">ជូនចំពោះ</p>
+                    <p className="font-[KhmerMoulLight] text-[18pt]">
+                      លោក {data.member.name_khmer}
+                    </p>
+                    <div className="font-[KhmerOSSiemreap] text-[12pt] text-center">
+                      <p className="">ដែលទទួលបានជ័យលាភី</p>
+                      <p className="">
+                        គម្រោងឆ្នើមលេខ{" "}
+                        {data.project.rank.toString().toKhmerDigit()}{" "}
+                        ក្រោមប្រធាមបទ
+                      </p>
+                      <span className="font-['serif'] text-[13pt] italic">
+                        &quot;{data.project.project_name}&quot;
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex flex-col justify-center items-center gap-1 w-[391px] h-[300px]">
-                <p className="font-[KhmerMoulLight] text-[16pt] pt-1">ទិវាវិស្វកម្មលើកទី៦</p>
-                <p className="font-[KhmerBTB] text-[13pt]">ជ័យលាភីឆ្នាំ២០២៣</p>
-                <p className="font-[KhmerMoulLight] text-[16pt]">ប័ណ្ណសរសើរ</p>
-                <p className="font-[KhmerBTB] text-[13pt]">ជូនចំពោះ</p>
-                <p className="font-[KhmerMoulLight] text-[18pt]">លោក សេន ស៊ុនណេង</p>
-                <div className="font-[KhmerOSSiemreap] text-[12pt] text-center">
-                  <p className="">ដែលទទួលបានជ័យលាភី</p>
-                  <p className="">គម្រោងឆ្នើមលេខ ១ ក្រោមប្រធាមបទ</p>
-                  <span className="font-['serif'] text-[13pt] italic">&quot;Aleo Vera: Production & Processing&quot;</span>
+                <div className="text-center pb-12 mt-1">
+                  <p className="font-[KhmerOSSiemreap] text-[11.54pt]">
+                    រាជធានីភ្នំពេញ ថ្ងៃទី២១ ខែវិច្ឆិកា ឆ្នាំ២០២៣
+                  </p>
+                  <p className="font-['serif'] text-[13.02pt] mb-2">
+                    Phnom Penh, 21 November 2023
+                  </p>
                 </div>
               </div>
+              <div className="text-start w-full px-[7.5rem] pt-[5rem] text-xs text-gray-400">
+                {" "}
+                {data.project.project_id}
+              </div>
             </div>
-            <div className="text-center pb-12 mt-1">
-              <p className="font-[KhmerOSSiemreap] text-[11.54pt]">រាជធានីភ្នំពេញ ថ្ងៃទី២១ ខែវិច្ឆិកា ឆ្នាំ២០២៣</p>
-              <p className="font-['serif'] text-[13.02pt] mb-2">Phnom Penh, 21 November 2023</p>
-            </div>
-          </div>
-            <div className="text-start w-full px-[7.5rem] pt-[5rem] text-xs text-gray-400"> <p>IG801</p></div>
-          </div>
-        </section>
+          </section>
+        ))}
       </div>
       <div className="flex justify-end w-[297mm]">
-      <button onClick={handleDownload} className="mt-4 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl hover:scale-105 focus:ring-4 focus:outline-none focus:ring-blue-30 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">{isDownloading ? 'Downloading...' : isDownloaded ? 'Download Complete!' : 'Download PDF'}</button>
+        <button
+          onClick={handleDownload}
+          className="mt-4 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl hover:scale-105 focus:ring-4 focus:outline-none focus:ring-blue-30 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          {isDownloading
+            ? "Downloading..."
+            : isDownloaded
+            ? "Download Complete!"
+            : "Download PDF"}
+        </button>
       </div>
     </div>
   );
 }
 
 export default HtmlToImageToPdf;
+
